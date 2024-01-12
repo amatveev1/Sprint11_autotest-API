9c0e31daf06d955a6a90ed5bb53ac4bcc12a74fe
amatveev1 <153431263+amatveev1@users.noreply.github.com>
2024-01-12 16:05:24 +0300

    Update check_order_track_test.py
    correct. delete kakie-to letters

--- check_order_track_test.py
+++ check_order_track_test.py
@@ -4,7 +4,7 @@ import order_request
 #Получить номер трека
 def get_track_order():
     response = order_request.post_new_orders()
-    return response.json()['track']свcd
+    return response.json()['track']
 
 #Проверить возможность запроса деталей заказа по номеру трека
 def test_order():
