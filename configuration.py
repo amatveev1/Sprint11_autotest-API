2d135c2e216c3012daa6fc990ac196ce891f0064
Anatoliy Matveev <TolyaN-2108@yandex.ru>
2024-01-15 11:57:04 +0300

    Comment added

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE ="https://63282cf1-9396-470b-8c5e-083a35f86f25.serverhub.praktikum-services.ru"
+URL_SERVICE ="https://1563a96e-8dba-4ae7-a948-5a7f71e3f4e3.serverhub.praktikum-services.ru"
 ORDERS_CREATE = "/api/v1/orders"
 TRACK_ORDERS = "/api/v1/orders/track"
